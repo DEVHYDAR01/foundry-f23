753d8e0a9ec5e0b368c67a1a71c87ce1ab70541f
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-09-05 01:06:47 +0100

    This commit will push simple bank solidity challenge contract

--- foundry-simple-bank-contract/src/SimpleBankContract.sol
+++ foundry-simple-bank-contract/src/SimpleBankContract.sol
@@ -13,9 +13,37 @@ pragma solidity ^0.8.19;
 // Emit an event whenever a deposit or withdrawal happens.
 
 contract SimpleBankContract {
-    mapping (address => uint) userToBalance;
-    function deposit() public payable {
-        userToBalance[msg.sender] += msg.value;
+    mapping (address => uint) public userToBalance;
+    address[] funders;
+    address public immutable owner;
+    
+    constructor () {
+        owner = msg.sender;
     }
 
+    function deposit() public payable {
+        userToBalance[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function withdraw(address _useraddr, uint256 _amount) public {
+        uint256 amountDeposited = userToBalance[_useraddr];
+        require(_amount <= amountDeposited, "You can't withdraw more than you deposited");
+
+        // Update state before transferring funds to prevent reentrancy
+        userToBalance[_useraddr] -= _amount;
+        require(address(this).balance >= _amount, "Not enough balance in contract");
+
+        // Transfer funds after updating the state
+        (bool Success, ) = payable(_useraddr).call{value: _amount}("");
+        require(Success, "Call failed");
+    }
+
+    function checkBalance() public view returns (uint256) {
+        return userToBalance[msg.sender];  // Returns the balance of the caller
+    }
+
+    function totalBankEth() public view returns (uint256) {
+        return address(this).balance;
+    }
 }
