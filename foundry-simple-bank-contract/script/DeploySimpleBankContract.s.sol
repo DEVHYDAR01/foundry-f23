0e97e9013f4e4fead6e65aaae6e8810df8ce3548
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-09-07 16:35:22 +0100

    This commit will push script to deploy simple bank contract

---
+++ foundry-simple-bank-contract/script/DeploySimpleBankContract.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from  "../lib/forge-std/src/Script.sol";
+import {SimpleBankContract} from "../src/SimpleBankContract.sol";
+
+contract DeploySimpleBankContract is Script {
+    function run() external {
+        vm.startBroadcast();
+        SimpleBankContract simplebankcontract = new SimpleBankContract();
+        vm.stopBroadcast();
+    }
+}
